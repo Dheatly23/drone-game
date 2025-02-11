e84518d4ab0f6dd88694dbb62ecea1b94c212a54
Dheatly23 <71598333+Dheatly23@users.noreply.github.com>
2025-02-11 11:09:58 +0700

    Optimize Level Controller Serialization/Deserialization

--- rust/wasm/level/level-controller/src/lib.rs
+++ rust/wasm/level/level-controller/src/lib.rs
@@ -1,7 +1,8 @@
 #![allow(clippy::deref_addrof)]
 
-use rkyv::api::high::{from_bytes, to_bytes_in};
+use rkyv::api::high::{from_bytes, to_bytes_in_with_alloc};
 use rkyv::rancor::Panic;
+use rkyv::ser::allocator::Arena;
 use rkyv::ser::writer::Buffer;
 
 use level_state::LevelState;
@@ -15,11 +16,14 @@ extern "C" {
 }
 
 const BUF_LEN: usize = 1024 * 1024;
-static mut BUFFER: [u8; BUF_LEN] = [0; BUF_LEN];
+#[repr(C, align(65536))]
+struct StaticBuffer([u8; BUF_LEN]);
+static mut BUFFER: StaticBuffer = StaticBuffer([0; BUF_LEN]);
+static mut ARENA: Option<Arena> = None;
 
 unsafe fn read() -> &'static [u8] {
-    let i = _read_data(&raw mut BUFFER as _, BUF_LEN as _);
-    &*(&raw const BUFFER[..i as usize])
+    let i = _read_data(&raw mut BUFFER.0 as _, BUF_LEN as _);
+    &*(&raw const BUFFER.0[..i as usize])
 }
 
 static mut LEVEL: LevelState = LevelState::new_empty();
@@ -40,8 +44,19 @@ pub extern "C" fn import() {
 
 #[unsafe(no_mangle)]
 pub extern "C" fn export() {
-    let (level, buf) = unsafe { (&*(&raw const LEVEL), &mut *(&raw mut BUFFER)) };
-    let buf = to_bytes_in::<_, Panic>(level, Buffer::from(buf)).unwrap();
+    let (level, buf, arena) = unsafe {
+        (
+            &*(&raw const LEVEL),
+            &mut *(&raw mut BUFFER.0),
+            &mut *(&raw mut ARENA),
+        )
+    };
+    let buf = to_bytes_in_with_alloc::<_, _, Panic>(
+        level,
+        Buffer::from(buf),
+        arena.get_or_insert_default().acquire(),
+    )
+    .unwrap();
     unsafe {
         _write_data(buf.as_ptr(), buf.len() as _);
     }
