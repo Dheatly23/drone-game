eb0c271aa16648de9e347b290bfc8c190ecbe804
Dheatly23 <71598333+Dheatly23@users.noreply.github.com>
2025-02-24 14:34:28 +0700

    Refactors Executor

--- rust/wasm/drone/drone-js/src/executor.rs
+++ rust/wasm/drone/drone-js/src/executor.rs
@@ -6,7 +6,7 @@ use std::sync::atomic::{AtomicBool, Ordering};
 use std::task::{Context, Poll, Wake, Waker};
 
 pub struct Executor<O> {
-    futures: RefCell<Vec<Entry<O>>>,
+    futures: RefCell<Vec<Option<Entry<O>>>>,
 }
 
 struct Entry<O> {
@@ -52,13 +52,13 @@ impl<O> Executor<O> {
             return Some(o);
         }
 
-        self.futures.borrow_mut().push(Entry { fut, flag });
+        self.futures.borrow_mut().push(Some(Entry { fut, flag }));
         None
     }
 
     pub fn run(&self) -> impl '_ + Iterator<Item = O> {
         struct It<'a, O> {
-            this: &'a Executor<O>,
+            this: Option<&'a Executor<O>>,
             ix: usize,
         }
 
@@ -66,10 +66,22 @@ impl<O> Executor<O> {
             type Item = O;
 
             fn next(&mut self) -> Option<Self::Item> {
+                let this = self.this?;
+
                 loop {
-                    let mut guard = self.this.futures.borrow_mut();
-                    self.ix = self.ix.min(guard.len()).checked_sub(1)?;
-                    let mut e = guard.swap_remove(self.ix);
+                    let mut guard = this.futures.borrow_mut();
+                    let (i, mut e) = loop {
+                        if self.ix >= guard.len() {
+                            guard.retain(Option::is_some);
+                            self.this = None;
+                            return None;
+                        }
+                        let i = self.ix;
+                        self.ix += 1;
+                        if let Some(e) = guard[i].take() {
+                            break (i, e);
+                        }
+                    };
                     drop(guard);
 
                     if e.flag.flag.swap(false, Ordering::SeqCst) {
@@ -78,19 +90,23 @@ impl<O> Executor<O> {
                             .as_mut()
                             .poll(&mut Context::from_waker(&Waker::from(e.flag.clone())))
                         {
-                            self.ix += 1;
                             return Some(o);
                         }
                     }
 
-                    self.this.futures.borrow_mut().push(e);
+                    guard = this.futures.borrow_mut();
+                    if let v @ None = &mut guard[i] {
+                        *v = Some(e);
+                    } else {
+                        guard.push(Some(e));
+                    }
                 }
             }
         }
 
         It {
-            ix: self.futures.borrow().len(),
-            this: self,
+            ix: 0,
+            this: Some(self),
         }
     }
 }
